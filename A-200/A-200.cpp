51b202bc4b1da15303f02a17b8516e5d16044321
John McQ <jdmccue27@gmail.com>
2018-01-16 22:52:57 -0500

    Almost back

--- A-200/A-200.cpp
+++ A-200/A-200.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 #include <SPI.h>
 #include <Ethernet.h>
 #include <DHT.h>
@@ -25,102 +24,52 @@ int reading;
 int TT_201_Pin = 1;
 float TT_201;
 
-
 // ----------------------   Ethernet setup ----------------
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 1, 52);
 IPAddress server(192, 168, 1, 50); //MQTT 'Broker'
 //EthernetServer Webserver(80);
 
-// ---------------  MQTT ------------------------
-
-//EthernetClient ethClient;
-MQTTClient MQclient;
-
-void callback(char* topic, byte* payload, unsigned int length) {
-//void callback(char* topic, byte* payload, int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (unsigned int i=0; i<length ;i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
-
-//   ----------------- End MQTT ---------------------------------
-
-//Celsius to Fahrenheit conversion
-double Fahrenheit(double celsius){
-  return 1.80 * celsius + 32.00;
-}
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
-}
-
-void connect() {
-  Serial.print("connecting...");
-  while (!MQclient.connect("ARD_200")) {
-    Serial.print(".");
-    delay(1000);
-  }
-
-  Serial.println("\nconnected!");
-
-  MQclient.subscribe("/hello");
-  // client.unsubscribe("/hello");MQ
-}
-
-
 
 void setup() {
 
-
     analogReference(INTERNAL);
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
     }
 
- //   Ethernet.begin(mac, ip);
- //   Webserver.begin();
-    Serial.print("server is at ");
-//    Serial.println(Ethernet.localIP());
-
-    pinMode(PIR_203_PIN, INPUT);
-
-//    MQclient.begin( "192.168.1.50", ethClient);
-    MQclient.onMessage(messageReceived);
-
-    connect();
-//  MQTT setup
-
-//    MQTTClient client;
-//    rc = MQTTClient_create(&client, "192.168.1.50:1883", "Ard_201" , MQTTCLIENT_PERSISTENCE_NONE, NULL);
-
-
 }
 
+int n = 0;
 
 void loop() {
 
-    delay(2000);
+	reading = analogRead(TT_201_Pin);
+	TT_201 = reading / 9.31;
+	int retcode = sprintf(DT_202_str, "%f",TT_201);
+ //    delay(2000);
 
      DH_202 = DHT_201.readHumidity();
      // Read temperature as Celsius (the default)
      //float t = dht.readTemperature();
      // Read temperature as Fahrenheit (isFahrenheit = true)
      DT_202 = DHT_201.readTemperature(true);
-
+     Serial.print("New Series: \n\r");
+     Serial.print("Formatted Temp: ");
+     Serial.print(DT_202_str); Serial.print("   Code: "); Serial.print(retcode);
+     Serial.print("\n\r");
+     Serial.print("\n\r");
+     Serial.print("Analog Temp: ");
+     Serial.print(TT_201);
+     Serial.print("\n\r");
      Serial.print("Temperature: ");
-     Serial.println(DT_202);
-     Serial.print("Humidity: ");
-     Serial.println(DH_202);
-
+     Serial.print(DT_202);
+     Serial.print("\n\r");
+     Serial.print("   Humidity: ");
+     Serial.print(DH_202);
+     Serial.print("\n\r");
+     Serial.print("\n\r");
      delay(2000);
-
-
 
 }
